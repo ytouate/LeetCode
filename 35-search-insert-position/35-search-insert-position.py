1d44d74d1245185fe7ac3d11d08c51d04527b7ae
ytouate <95024105+ytouate@users.noreply.github.com>
2022-04-20 11:49:45 +0000

    Time: 42 ms (57.84%), Space: 14.2 MB (26.03%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,11 +1,3 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        i = 0
-        while i < len(nums):
-            if nums[i] >= target:
-                return i
-            i += 1
-        return len(nums)
 
 class Solution(object):
     def searchInsert(self, nums, target):
