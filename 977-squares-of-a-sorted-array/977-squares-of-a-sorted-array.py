9c11572c33770f710f77bfead175fdb56b4b99a4
ytouate <95024105+ytouate@users.noreply.github.com>
2022-04-22 02:26:41 +0000

    Time: 486 ms (6.39%), Space: 15.6 MB (54.14%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -6,12 +6,12 @@ class Solution(object):
         right = len(nums) - 1
         j = len(nums) - 1
         while left <= right:
-            if abs(nums[left]) * abs(nums[left]) > abs(nums[right]) * abs(nums[right]):
-                temp.insert(0, abs(nums[left]) * abs(nums[left]))
+            if nums[left] * nums[left] > nums[right] * nums[right]:
+                temp.insert(0, nums[left] * nums[left])
                 left += 1
                 j -= 1
             else:
-                temp.insert(0, abs(nums[right]) * abs(nums[right]))
+                temp.insert(0, nums[right] * nums[right])
                 right -= 1
                 j -= 1
         return temp
