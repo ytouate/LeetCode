24c691d37a8c8461ef28f0c1ceacbbea40a26be4
ytouate <95024105+ytouate@users.noreply.github.com>
2022-05-12 10:10:55 +0100

    Time: 244 ms (46.72%), Space: 15.4 MB (73.87%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -1,18 +1,5 @@
 import math
 class Solution(object):
     def sortedSquares(self, nums):
-        left = 0
-        temp = []
-        right = len(nums) - 1
-        j = len(nums) - 1
-        while left <= right:
-            if nums[left] * nums[left] > nums[right] * nums[right]:
-                temp.insert(0, nums[left] * nums[left])
-                left += 1
-                j -= 1
-            else:
-                temp.insert(0, nums[right] * nums[right])
-                right -= 1
-                j -= 1
-        return temp
-            
+        a = sorted(list(map(lambda a : a * a, nums)))
+        return (a)
